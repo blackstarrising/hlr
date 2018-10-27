b2cabf057ed2543d3c1d2ef7d9260b7daaf529ee
Finn Labe <labe@wr.informatik.uni-hamburg.de>
2018-10-27 23:30:43 +0200

    kacke

--- blatt02/simple/simple.c
+++ blatt02/simple/simple.c
@@ -9,7 +9,7 @@
 int *
 mistake1 ()
 {
-  int buf[] = { 1, 1, 2, 3, 4, 5 };
+  static int buf[] = { 1, 1, 2, 3, 4, 5 }; //static hinzugefügt um lokale Variable noch von außerhalb von mistake1 aufrufen zu können
   return buf;
 }
 
@@ -17,7 +17,7 @@ int *
 mistake2 ()
 {
   int *buf = malloc (sizeof (char) * 4);
-  buf[2] = 2;
+  buf[1] = 2; //hier wurde die Stelle des Arrays gewechselt weil nachher die [1] abgefragt wird.
   return buf;
 }
 
@@ -25,8 +25,8 @@ int *
 mistake3 ()
 {
   /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
-  int mistake2_ = 0;
-  int *buf = (int *) &mistake2;
+  int *foo = malloc (sizeof (int));
+  int *buf = foo; //Was soll das hier eigtentlich!
   buf[0] = 3;
   return buf;
 }
@@ -35,8 +35,7 @@ int *
 mistake4 ()
 {
   int *buf = malloc (sizeof (char) * 4);
-  buf[4] = 4;
-  free (buf);
+  buf[0] = 4;
   return buf;
 }
 
@@ -53,7 +52,10 @@ main (void)
 
   /* mhh muss hier noch etwas gefreed werden? */
   /* Fügen sie hier die korrekten aufrufe von free() ein */
-  free (p[1]);			/* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
-
+  //free (p[1]);
+  /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
+  //free (p[2]);
+  //free (p[3]);
+  
   return 0;
 }
