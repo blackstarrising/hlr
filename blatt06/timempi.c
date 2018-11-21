f8871a5346aeb2e06b82170c7372a08ae3b3e832
Finn Labe <labe@wr.informatik.uni-hamburg.de>
2018-11-21 16:48:44 +0100

    finished task 1

--- blatt06/timempi.c
+++ blatt06/timempi.c
@@ -2,35 +2,64 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
   //Initialisiere MPI
   MPI_Init(NULL, NULL);
 
+  //Lese Anzahl Prozesse aus
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  
   //Lese Prozessrang aus
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int namelen;
-  MPI_Get_processor_name(processor_name, &namelen);
+  if (world_size < 2)
+    {
+      MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
-  time_t rawtime;
-  struct tm *time_info;
-  char time_buffer[80];
+  char outputstring[50];  
 
-  time(&rawtime);
-  time_info = localtime(&rawtime);
-  strftime(time_buffer, 80, "%x %X", time_info);
+  //MPI_Barrier(MPI_COMM_WORLD);
 
-  struct timeval exacttime;
-  gettimeofday(&exacttime, NULL);
+  if(world_rank == 0)
+    {      
+      //Prozess 0 empfängt die Strings aller anderen Prozesse.
+      for (int j = 1; j < world_size; j++){
+	MPI_Recv(outputstring, 50, MPI_CHAR, j, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("%s\n", outputstring);
+      }
+    }
+  else
+    {
+      //Generiere Prozessorname
+      char processor_name[MPI_MAX_PROCESSOR_NAME];
+      int namelen;
+      MPI_Get_processor_name(processor_name, &namelen);
 
-  char* outputstring;
-  sprintf(outputstring, "%s: %s.%ld - rank: %d \n", processor_name, time_buffer, exacttime.tv_usec, world_rank);
-  printf(outputstring);
-  
+      //Generiere Timestamp
+      time_t rawtime;
+      struct tm *time_info;
+      char time_buffer[80];
+      time(&rawtime);
+      time_info = localtime(&rawtime);
+      strftime(time_buffer, 30, "%x %X", time_info);
+      struct timeval exacttime;
+      gettimeofday(&exacttime, NULL);
+
+      //Generiere Outputstring
+      sprintf(outputstring, "%s: %s.%ld", processor_name, time_buffer, exacttime.tv_usec);
+      //sprintf(outputstring,"test");
+
+      //Sende Outputstring an Prozess 0
+      MPI_Send(outputstring, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+  MPI_Barrier(MPI_COMM_WORLD);
+  printf("Rang %i beendet jetzt!\n", world_rank);
   MPI_Finalize();
 }
