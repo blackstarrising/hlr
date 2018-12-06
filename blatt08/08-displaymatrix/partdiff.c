e1e87a5742fe53a7cd03c79bd080a125822aab09
Finn Labe <labe@wr.informatik.uni-hamburg.de>
2018-12-06 15:19:37 +0100

    Matrix initialisation prepared

--- blatt08/08-displaymatrix/partdiff.c
+++ blatt08/08-displaymatrix/partdiff.c
@@ -32,18 +32,21 @@
 
 struct calculation_arguments
 {
-	uint64_t  N;              /* number of spaces between lines (lines=N+1)     */
-	uint64_t  num_matrices;   /* number of matrices                             */
-	double    h;              /* length of a space between two lines            */
-	double    ***Matrix;      /* index matrix used for addressing M             */
-	double    *M;             /* two matrices with real values                  */
+  uint64_t  N;              /* number of spaces between lines (lines=N+1)     */
+  uint64_t  num_matrices;   /* number of matrices                             */
+  double    h;              /* length of a space between two lines            */
+  double    ***Matrix;      /* index matrix used for addressing M             */
+  double    *M;             /* two matrices with real values                  */
+  uint64_t  rank;           /* Rank of the current process                    */
+  uint64_t  world_size;     /* Number of Processes                            */
+  uint64_t  Nh;             /* Number of Lines of a single process Matrix     */
 };
 
 struct calculation_results
 {
-	uint64_t  m;
-	uint64_t  stat_iteration; /* number of current iteration                    */
-	double    stat_precision; /* actual precision of all slaves in iteration    */
+  uint64_t  m;
+  uint64_t  stat_iteration; /* number of current iteration                    */
+  double    stat_precision; /* actual precision of all slaves in iteration    */
 };
 
 /* ************************************************************************ */
@@ -62,13 +65,23 @@ static
 void
 initVariables (struct calculation_arguments* arguments, struct calculation_results* results, struct options const* options)
 {
-	arguments->N = (options->interlines * 8) + 9 - 1;
-	arguments->num_matrices = (options->method == METH_JACOBI) ? 2 : 1;
-	arguments->h = 1.0 / arguments->N;
+  uint64_t N = (options->interlines * 8) + 9 - 1;
+  arguments->N = N;
+  arguments->num_matrices = (options->method == METH_JACOBI) ? 2 : 1;
+  arguments->h = 1.0 / arguments->N;
+  
+  results->m = 0;
+  results->stat_iteration = 0;
+  results->stat_precision = 0;
+  
+  uint64_t world_size = arguments->world_size;
+  uint64_t rank = arguments->rank;
 
-	results->m = 0;
-	results->stat_iteration = 0;
-	results->stat_precision = 0;
+  // Nh gibt die Anzahl Zeilen im Speicherblock eines individuellen Prozesses an
+  // Nh = (N-1)/world_size (+1 wenn rank<(N-1)%world_size)
+  uint64_t Nh = (N-1)/world_size;
+  if(rank < ((N-1) % world_size)){Nh++;}
+  arguments->Nh=Nh;
 }
 
 /* ************************************************************************ */
@@ -115,22 +128,24 @@ static
 void
 allocateMatrices (struct calculation_arguments* arguments)
 {
-	uint64_t i, j;
+  uint64_t i, j;
 
-	uint64_t const N = arguments->N;
-
-	arguments->M = allocateMemory(arguments->num_matrices * (N + 1) * (N + 1) * sizeof(double));
-	arguments->Matrix = allocateMemory(arguments->num_matrices * sizeof(double**));
-
-	for (i = 0; i < arguments->num_matrices; i++)
+  uint64_t const N = arguments->N;
+  uint64_t const Nh = arguments->Nh;
+  
+  // Anstatt einer (N+1)x(N+1) Matrix bekommt jeder Prozess zwar eine N+1 breite, aber nicht hohe Matrix. Die Höhe ist durch Nh (siehe InitVariables) gegeben
+  arguments->M = allocateMemory(arguments->num_matrices * (N + 1) * (Nh + 2) * sizeof(double));
+  arguments->Matrix = allocateMemory(arguments->num_matrices * sizeof(double**));
+  
+  for (i = 0; i < arguments->num_matrices; i++)
+    {
+      arguments->Matrix[i] = allocateMemory((N + 1) * sizeof(double*));
+      
+      for (j = 0; j < (Nh+2); j++)
 	{
-		arguments->Matrix[i] = allocateMemory((N + 1) * sizeof(double*));
-
-		for (j = 0; j <= N; j++)
-		{
-			arguments->Matrix[i][j] = arguments->M + (i * (N + 1) * (N + 1)) + (j * (N + 1));
-		}
+	  arguments->Matrix[i][j] = arguments->M + (i * (N + 1) * (Nh + 2)) + (j * (N + 1));
 	}
+    }
 }
 
 /* ************************************************************************ */
@@ -140,41 +155,47 @@ static
 void
 initMatrices (struct calculation_arguments* arguments, struct options const* options)
 {
-	uint64_t g, i, j;                                /*  local variables for loops   */
+  uint64_t g, i, j;                                /*  local variables for loops   */
 
-	uint64_t const N = arguments->N;
-	double const h = arguments->h;
-	double*** Matrix = arguments->Matrix;
-
-	/* initialize matrix/matrices with zeros */
-	for (g = 0; g < arguments->num_matrices; g++)
+  uint64_t const rank = arguments->rank;
+  uint64_t const world_size = arguments->world_size;
+  uint64_t const N = arguments->N;
+  uint64_t const Nh = arguments->Nh;
+  double const h = arguments->h;
+  double*** Matrix = arguments->Matrix;
+  
+  /* initialize matrix/matrices with zeros */
+  for (g = 0; g < arguments->num_matrices; g++)
+    {
+      for (i = 0; i <= N; i++)
 	{
-		for (i = 0; i <= N; i++)
-		{
-			for (j = 0; j <= N; j++)
-			{
-				Matrix[g][i][j] = 0.0;
-			}
-		}
+	  for (j = 0; j < (Nh+2); j++)
+	    {
+	      Matrix[g][i][j] = 0.0;
+	    }
 	}
-
-	/* initialize borders, depending on function (function 2: nothing to do) */
-	if (options->inf_func == FUNC_F0)
+    }
+  
+  /* initialize borders, depending on function (function 2: nothing to do) */
+  if (options->inf_func == FUNC_F0)
+    {
+      for (g = 0; g < arguments->num_matrices; g++)
 	{
-		for (g = 0; g < arguments->num_matrices; g++)
-		{
-			for (i = 0; i <= N; i++)
-			{
-				Matrix[g][i][0] = 1.0 - (h * i);
-				Matrix[g][i][N] = h * i;
-				Matrix[g][0][i] = 1.0 - (h * i);
-				Matrix[g][N][i] = h * i;
-			}
-
-			Matrix[g][N][0] = 0.0;
-			Matrix[g][0][N] = 0.0;
-		}
+	  for (i = 0; i <= N; i++)// Setze die Oberste Zeile der obersten Prozesses (rank = 0) und die unterste des untersten (rank = world_size-1)
+	    {
+	      if(rank == 0){Matrix[g][0][i] = 1.0 - (h * i);}
+	      if(rank == world_size-1){Matrix[g][N][i] = h * i;}
+	    }
+	  for (i = 1; i <= Nh; i++)
+	    {
+	      Matrix[g][i][0] = 1.0 - (h * (i+Nh*rank));
+	      Matrix[g][i][N] = h * (i+Nh*rank);
+	    }
+	  
+	  Matrix[g][N][0] = 0.0;
+	  Matrix[g][0][N] = 0.0;
 	}
+    }
 }
 
 /* ************************************************************************ */
@@ -485,7 +506,7 @@ main (int argc, char** argv)
   struct calculation_arguments arguments;
   struct calculation_results results;
 
-  //Brauch eigenen MPI_Struct um die Optionen weiterzugeben zu können!
+  //Brauch eigenen MPI-Datentypen MPI_Struct_options um die Optionen weiterzugeben zu können!
   MPI_Datatype MPI_Struct_options;
   int structlen = 7;
   int blocklengths[structlen];
@@ -514,9 +535,13 @@ main (int argc, char** argv)
       AskParams(&options, argc, argv);
     }
   MPI_Bcast(&options, 1, MPI_Struct_options, 0, MPI_COMM_WORLD);
-  
+
+  //Läuft Problemlos parallel, muss nur rank und world_size noch reinschreiben
+  arguments.rank = rank;
+  arguments.world_size = world_size;
   initVariables(&arguments, &results, &options);
-  
+
+  //Muss parallelisiert und die Speicdhervergabe muss angepasst werden
   allocateMatrices(&arguments);
   initMatrices(&arguments, &options);
   
