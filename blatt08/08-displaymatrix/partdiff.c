f17e15ecd3b7bbae8648eae213223a2dbf1b5cf3
Niklas Kaeming <kaeming@wr.informatik.uni-hamburg.de>
2018-12-08 20:29:01 +0100

    Finns Version ohne OpenMP für Blatt 8 in den master geladen.

--- blatt08/08-displaymatrix/partdiff.c
+++ blatt08/08-displaymatrix/partdiff.c
@@ -1,4 +1,4 @@
-#/****************************************************************************/
+/****************************************************************************/
 /****************************************************************************/
 /**                                                                        **/
 /**                 TU München - Institut für Informatik                   **/
@@ -18,6 +18,7 @@
 /* Include standard header file.                                            */
 /* ************************************************************************ */
 #define _POSIX_C_SOURCE 200809L
+#define TAG_PLACEHOLDER 1 //If Tags are needed this can be used.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,18 +33,21 @@
 
 struct calculation_arguments
 {
-	uint64_t  N;              /* number of spaces between lines (lines=N+1)     */
-	uint64_t  num_matrices;   /* number of matrices                             */
-	double    h;              /* length of a space between two lines            */
-	double    ***Matrix;      /* index matrix used for addressing M             */
-	double    *M;             /* two matrices with real values                  */
+  uint64_t  N;              /* number of spaces between lines (lines=N+1)     */
+  uint64_t  num_matrices;   /* number of matrices                             */
+  double    h;              /* length of a space between two lines            */
+  double    ***Matrix;      /* index matrix used for addressing M             */
+  double    *M;             /* two matrices with real values                  */
+  uint64_t  rank;           /* Rank of the current process                    */
+  uint64_t  world_size;     /* Number of Processes                            */
+  uint64_t  Nh;             /* Number of Lines of a single process Matrix     */
 };
 
 struct calculation_results
 {
-	uint64_t  m;
-	uint64_t  stat_iteration; /* number of current iteration                    */
-	double    stat_precision; /* actual precision of all slaves in iteration    */
+  uint64_t  m;
+  uint64_t  stat_iteration; /* number of current iteration                    */
+  double    stat_precision; /* actual precision of all slaves in iteration    */
 };
 
 /* ************************************************************************ */
@@ -62,13 +66,23 @@ static
 void
 initVariables (struct calculation_arguments* arguments, struct calculation_results* results, struct options const* options)
 {
-	arguments->N = (options->interlines * 8) + 9 - 1;
-	arguments->num_matrices = (options->method == METH_JACOBI) ? 2 : 1;
-	arguments->h = 1.0 / arguments->N;
+  uint64_t N = (options->interlines * 8) + 9 - 1;
+  arguments->N = N;
+  arguments->num_matrices = (options->method == METH_JACOBI) ? 2 : 1;
+  arguments->h = 1.0 / arguments->N;
+  
+  results->m = 0;
+  results->stat_iteration = 0;
+  results->stat_precision = 0;
+  
+  uint64_t world_size = arguments->world_size;
+  uint64_t rank = arguments->rank;
 
-	results->m = 0;
-	results->stat_iteration = 0;
-	results->stat_precision = 0;
+  // Nh gibt die Anzahl Zeilen im Speicherblock eines individuellen Prozesses an
+  // Nh = (N-1)/world_size (+1 wenn rank<(N-1)%world_size)
+  uint64_t Nh = (N-1)/world_size;
+  if(rank < ((N-1) % world_size)){Nh++;}
+  arguments->Nh=Nh;
 }
 
 /* ************************************************************************ */
@@ -115,22 +129,23 @@ static
 void
 allocateMatrices (struct calculation_arguments* arguments)
 {
-	uint64_t i, j;
+  uint64_t i, j;
 
-	uint64_t const N = arguments->N;
+  uint64_t const N = arguments->N;
+  uint64_t const Nh = arguments->Nh;
+  
+  // Anstatt einer (N+1)x(N+1) Matrix bekommt jeder Prozess zwar eine N+1 breite, aber nicht hohe Matrix. Die Höhe ist durch Nh + 2 (siehe InitVariables) gegeben
+  arguments->M = allocateMemory(arguments->num_matrices * (N + 1) * (Nh + 2) * sizeof(double));
+  arguments->Matrix = allocateMemory(arguments->num_matrices * sizeof(double**));
 
-	arguments->M = allocateMemory(arguments->num_matrices * (N + 1) * (N + 1) * sizeof(double));
-	arguments->Matrix = allocateMemory(arguments->num_matrices * sizeof(double**));
-
-	for (i = 0; i < arguments->num_matrices; i++)
+  for (i = 0; i < arguments->num_matrices; i++)
+    {
+      arguments->Matrix[i] = allocateMemory((N + 1) * sizeof(double*));
+      for (j = 0; j < (Nh+2); j++)
 	{
-		arguments->Matrix[i] = allocateMemory((N + 1) * sizeof(double*));
-
-		for (j = 0; j <= N; j++)
-		{
-			arguments->Matrix[i][j] = arguments->M + (i * (N + 1) * (N + 1)) + (j * (N + 1));
-		}
+	  arguments->Matrix[i][j] = arguments->M + (i * (N + 1) * (Nh + 2)) + (j * (N + 1));
 	}
+    }
 }
 
 /* ************************************************************************ */
@@ -140,46 +155,56 @@ static
 void
 initMatrices (struct calculation_arguments* arguments, struct options const* options)
 {
-	uint64_t g, i, j;                                /*  local variables for loops   */
+  uint64_t g, i, j;                                /*  local variables for loops   */
 
-	uint64_t const N = arguments->N;
-	double const h = arguments->h;
-	double*** Matrix = arguments->Matrix;
-
-	/* initialize matrix/matrices with zeros */
-	for (g = 0; g < arguments->num_matrices; g++)
+  uint64_t const rank = arguments->rank;
+  uint64_t const world_size = arguments->world_size;
+  uint64_t const N = arguments->N;
+  uint64_t const Nh = arguments->Nh;
+  double const h = arguments->h;
+  double*** Matrix = arguments->Matrix;
+  
+  /* initialize matrix/matrices with zeros */
+  for (g = 0; g < arguments->num_matrices; g++)
+    {
+      for (i = 0; i < (Nh+2); i++)
 	{
-		for (i = 0; i <= N; i++)
-		{
-			for (j = 0; j <= N; j++)
-			{
-				Matrix[g][i][j] = 0.0;
-			}
-		}
+	  for (j = 0; j <= N; j++)
+	    {
+	      Matrix[g][i][j] = 0.0;
+	    }
 	}
+    }
 
-	/* initialize borders, depending on function (function 2: nothing to do) */
-	if (options->inf_func == FUNC_F0)
+  /* initialize borders, depending on function (function 2: nothing to do) */
+  if (options->inf_func == FUNC_F0)
+    {
+      // Brauche Offset für die vertikalen Kanten, da diese nicht in einem gemeinsamen speicher liegen.
+      int offset = ((N-1)/world_size*rank);
+      if((N-1)%world_size <= rank){offset += (N-1)%world_size;}
+      else if(rank < (N-1)%world_size){offset += rank;}
+      for (g = 0; g < arguments->num_matrices; g++)
 	{
-		for (g = 0; g < arguments->num_matrices; g++)
-		{
-			for (i = 0; i <= N; i++)
-			{
-				Matrix[g][i][0] = 1.0 - (h * i);
-				Matrix[g][i][N] = h * i;
-				Matrix[g][0][i] = 1.0 - (h * i);
-				Matrix[g][N][i] = h * i;
-			}
-
-			Matrix[g][N][0] = 0.0;
-			Matrix[g][0][N] = 0.0;
-		}
+	  for (i = 0; i <= N; i++)// Setze die Oberste Zeile der obersten Prozesses (rank = 0) und die unterste des untersten (rank = world_size-1)
+	    {
+	      if(rank == 0){Matrix[g][0][i] = 1.0 - (h * i);}
+	      if(rank == world_size-1){Matrix[g][Nh+1][i] = h * i;}
+	    }
+	  for (i = 1; i <= Nh; i++)
+	    {
+	      Matrix[g][i][0] = 1.0 - (h * (i+offset));
+	      Matrix[g][i][N] = h * (i+offset);
+	    }
+	  Matrix[g][Nh+1][0] = 0.0;
+	  Matrix[g][0][N] = 0.0;
 	}
+    }
 }
 
 /* ************************************************************************ */
 /* calculate: solves the equation                                           */
 /* ************************************************************************ */
+//Left the old function as required. Is also still used for one process.
 static
 void
 calculate (struct calculation_arguments const* arguments, struct calculation_results* results, struct options const* options)
@@ -291,15 +316,20 @@ calculateJacobiMPI (struct calculation_arguments const* arguments, struct calcul
   double residuum;                            /* residuum of current iteration */
   double maxresiduum;                         /* maximum residuum value of a slave in iteration */
   
+  uint64_t const rank = arguments->rank;
+  uint64_t const world_size = arguments->world_size;
+  int const Nh = arguments->Nh;
+  
   int const N = arguments->N;
   double const h = arguments->h;
-  
+    
   double pih = 0.0;
   double fpisin = 0.0;
   
   int term_iteration = options->term_iteration;
   
   /* initialize m1 and m2 depending on algorithm */
+  //Legacy, kann an sich weg da sowieso je nach Methode ein anderes calculate (und andere Speicherstrukturen) verwendet werden.
   if (options->method == METH_JACOBI)
     {
       m1 = 0;
@@ -325,7 +355,7 @@ calculateJacobiMPI (struct calculation_arguments const* arguments, struct calcul
       maxresiduum = 0;
 
       /* over all rows */
-      for (i = 1; i < N; i++)
+      for (i = 1; i <= Nh; i++)
 	{
 	  double fpisin_i = 0.0;
 
@@ -361,19 +391,47 @@ calculateJacobiMPI (struct calculation_arguments const* arguments, struct calcul
       /* exchange m1 and m2 */
       i = m1;
       m1 = m2;
-      m2 = i;
+      m2 = i;      
 
       /* check for stopping calculation depending on termination method */
       if (options->termination == TERM_PREC)
 	{
-	  if (maxresiduum < options->term_precision)
+	  // Ein Prozess muss mit Reduce das gesamte MaxRedisuum berechnen und dann mit den anderen Teilen, ob abgebrochen werden muss
+	  double absolute_maxresiduum;
+	  MPI_Reduce(&maxresiduum, &absolute_maxresiduum, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	  if ((absolute_maxresiduum < options->term_precision) && (rank == 0))
 	    {
 	      term_iteration = 0;
 	    }
+	  MPI_Bcast(&term_iteration, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	}
       else if (options->termination == TERM_ITER)
 	{
 	  term_iteration--;
+	}
+
+      //Each Process:
+      //Recieve top border from above
+      //Send own first line to above
+      //Send own last line to below
+      //Recieve bottom border from below
+      //(Ausnahme: erster und letzter Prozess: erster nur nach unten kommunizieren, letzter nur nach oben!)
+      if(rank == 0)
+	{
+	  MPI_Send(Matrix_Out[Nh], N+1, MPI_DOUBLE, rank+1, TAG_PLACEHOLDER, MPI_COMM_WORLD);
+	  MPI_Recv(Matrix_Out[Nh+1], N+1, MPI_DOUBLE, rank+1, TAG_PLACEHOLDER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+      else if(rank == world_size-1)
+	{
+	  MPI_Recv(Matrix_Out[0], N+1, MPI_DOUBLE, rank-1, TAG_PLACEHOLDER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	  MPI_Send(Matrix_Out[1], N+1, MPI_DOUBLE, rank-1, TAG_PLACEHOLDER, MPI_COMM_WORLD);
+	}
+      else
+	{
+	  MPI_Recv(Matrix_Out[0], N+1, MPI_DOUBLE, rank-1, TAG_PLACEHOLDER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	  MPI_Send(Matrix_Out[1], N+1, MPI_DOUBLE, rank-1, TAG_PLACEHOLDER, MPI_COMM_WORLD);
+	  MPI_Send(Matrix_Out[Nh], N+1, MPI_DOUBLE, rank+1, TAG_PLACEHOLDER, MPI_COMM_WORLD);
+	  MPI_Recv(Matrix_Out[Nh+1], N+1, MPI_DOUBLE, rank+1, TAG_PLACEHOLDER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
     }
 
@@ -446,7 +504,8 @@ displayStatistics (struct calculation_arguments const* arguments, struct calcula
 /****************************************************************************/
 static
 void
-DisplayMatrix (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options)
+DisplayMatrixLEGACY (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options)
+//Alt, nur noch in der sequentiellen Variante genutzt.
 {
 	int x, y;
 
@@ -469,6 +528,87 @@ DisplayMatrix (struct calculation_arguments* arguments, struct calculation_resul
 	fflush (stdout);
 }
 
+/**
+ * rank and size are the MPI rank and size, respectively.
+ * from and to denote the global(!) range of lines that this process is responsible for.
+ *
+ * Example with 9 matrix lines and 4 processes:
+ * - rank 0 is responsible for 1-2, rank 1 for 3-4, rank 2 for 5-6 and rank 3 for 7.
+ *   Lines 0 and 8 are not included because they are not calculated.
+ * - Each process stores two halo lines in its matrix (except for ranks 0 and 3 that only store one).
+ * - For instance: Rank 2 has four lines 0-3 but only calculates 1-2 because 0 and 3 are halo lines for other processes. It is responsible for (global) lines 5-6.
+ */
+static
+void
+DisplayMatrix (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options, int rank, int size, int from, int to)
+{
+  int const elements = 8 * options->interlines + 9;
+
+  int x, y;
+  double** Matrix = arguments->Matrix[results->m];
+  MPI_Status status;
+
+  /* first line belongs to rank 0 */
+  if (rank == 0)
+    from--;
+
+  /* last line belongs to rank size - 1 */
+  if (rank + 1 == size)
+    to++;
+
+  if (rank == 0)
+    printf("Matrix:\n");
+
+  for (y = 0; y < 9; y++)
+  {
+    int line = y * (options->interlines + 1);
+
+    if (rank == 0)
+    {
+      /* check whether this line belongs to rank 0 */
+      if (line < from || line > to)
+      {
+        /* use the tag to receive the lines in the correct order
+         * the line is stored in Matrix[0], because we do not need it anymore */
+        MPI_Recv(Matrix[0], elements, MPI_DOUBLE, MPI_ANY_SOURCE, 42 + y, MPI_COMM_WORLD, &status);
+      }
+    }
+    else
+    {
+      if (line >= from && line <= to)
+      {
+        /* if the line belongs to this process, send it to rank 0
+         * (line - from + 1) is used to calculate the correct local address */
+        MPI_Send(Matrix[line - from + 1], elements, MPI_DOUBLE, 0, 42 + y, MPI_COMM_WORLD);
+      }
+    }
+
+    if (rank == 0)
+    {
+      for (x = 0; x < 9; x++)
+      {
+        int col = x * (options->interlines + 1);
+
+        if (line >= from && line <= to)
+        {
+          /* this line belongs to rank 0 */
+          printf("%7.4f", Matrix[line][col]);
+        }
+        else
+        {
+          /* this line belongs to another rank and was received above */
+          printf("%7.4f", Matrix[0][col]);
+        }
+      }
+
+      printf("\n");
+    }
+  }
+
+  fflush(stdout);
+}
+
+
 /* ************************************************************************ */
 /*  main                                                                    */
 /* ************************************************************************ */
@@ -485,7 +625,14 @@ main (int argc, char** argv)
   struct calculation_arguments arguments;
   struct calculation_results results;
 
-  //Brauch eigenen MPI_Struct um die Optionen weiterzugeben zu können!
+  //Fange Gauß-Seidel mit mehr als einem Prozess ab, da noch nicht parallelisiert:
+  if (options.method == METH_GAUSS_SEIDEL && world_size > 1)
+    {
+      if (rank == 0){printf("Aborted: Please use only one Process for Gauß-Seidel Method!\n");}
+      return 1;
+    }
+  
+  //Brauch eigenen MPI-Datentypen MPI_Struct_options um die Optionen weiterzugeben zu können!
   MPI_Datatype MPI_Struct_options;
   int structlen = 7;
   int blocklengths[structlen];
@@ -514,18 +661,38 @@ main (int argc, char** argv)
       AskParams(&options, argc, argv);
     }
   MPI_Bcast(&options, 1, MPI_Struct_options, 0, MPI_COMM_WORLD);
-  
+
+  //Läuft Problemlos parallel, muss nur rank und world_size noch reinschreiben
+  arguments.rank = rank;
+  arguments.world_size = world_size;
   initVariables(&arguments, &results, &options);
   
+  //Muss parallelisiert und die Speicdhervergabe muss angepasst werden
   allocateMatrices(&arguments);
+  MPI_Barrier(MPI_COMM_WORLD);
   initMatrices(&arguments, &options);
-  
+
+  //Für nur einen Prozess wird die alte calculate funktion genutzt.
+  MPI_Barrier(MPI_COMM_WORLD);
   gettimeofday(&start_time, NULL);
-  calculate(&arguments, &results, &options);
+  if(world_size == 1){calculate(&arguments, &results, &options);}
+  else{calculateJacobiMPI(&arguments, &results, &options);}
   gettimeofday(&comp_time, NULL);
-  
-  displayStatistics(&arguments, &results, &options);
-  DisplayMatrix(&arguments, &results, &options);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  //Calculate from and to for printing. Very similar to offset used in initMatrices
+  int N = arguments.N;
+  int from = ((N-1)/world_size*rank) + 1;
+  if((N-1)%world_size <= rank){from += (N-1)%world_size;}
+  else if(rank < (N-1)%world_size){from += rank;}
+  int to = from + arguments.Nh;
+
+  //Use old print for one process
+  if(rank == 0){displayStatistics(&arguments, &results, &options);}
+  if(world_size == 1){DisplayMatrixLEGACY(&arguments, &results, &options);}
+  else{DisplayMatrix(&arguments, &results, &options, rank, world_size, from, to);}
+
+  MPI_Barrier(MPI_COMM_WORLD);
   
   freeMatrices(&arguments);
 
